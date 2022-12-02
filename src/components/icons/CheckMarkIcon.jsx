25ec8105df99dbf5f30cbe558314ec4b5b992e48
Mohammad_shahzaib07 <shahzaib@quolam.com>
2022-12-02 23:44:47 +0530

    added icons

---
+++ src/components/icons/CheckMarkIcon.jsx
@@ -0,0 +1,19 @@
+function CheckMarkIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={20}
+      height={15}
+      viewBox="0 0 20 15"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M.25 7.321l7.27 6.491a.749.749 0 001.058-.059l11.23-12.501A.748.748 0 1018.692.251L7.961 12.196 1.25 6.202a.748.748 0 10-1 1.119z"
+        fill="#fff"
+      />
+    </svg>
+  );
+}
+
+export default CheckMarkIcon;
