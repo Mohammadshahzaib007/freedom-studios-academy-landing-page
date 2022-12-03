f36afd0cebe9baa9190403c87c01b5154eff2ef2
Mohammad_shahzaib07 <shahzaib@quolam.com>
2022-12-03 19:56:29 +0530

    hero section completed

--- src/Pages/Home/components/HeroSection.jsx
+++ src/Pages/Home/components/HeroSection.jsx
@@ -1,5 +1,31 @@
+import styles from "./heroSection.module.css";
+import logo from "../../../assets/logo.png";
+import Text from "../../../components/Text/Text";
+import Button from "../../../components/Button/Button";
+
 function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <section className={styles.section}>
+      <div className={`container ${styles.contentContainer}`}>
+        <img src={logo} alt="logo" />
+
+        <div className={styles.textContainer}>
+          <Text type="h4" style={{ marginBottom: "0.3125rem" }}>
+            first semester
+          </Text>
+          <Text type="h1" style={{ marginBottom: "0.625rem" }}>
+            freedom studios academy
+          </Text>
+          <Text type="body1" style={{ marginBottom: "1.875rem" }}>
+            Taking the message of freedom to the end of the earth through media
+            and representing christ in every way.
+          </Text>
+
+          <Button>Select a membership plan</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default HeroSection;
--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,39 +1,57 @@
 import styles from "./text.module.css";
 
 function Text(props) {
-  const { type, children } = props;
+  const { type, children, style } = props;
 
   switch (type) {
     case "h1":
       return (
-        <h1 className={`${styles.common} ${styles.heading1}`}>{children}</h1>
+        <h1 className={`${styles.common} ${styles.heading1}`} style={style}>
+          {children}
+        </h1>
       );
 
     case "h2":
       return (
-        <h2 className={`${styles.common} ${styles.heading2}`}>{children}</h2>
+        <h2 className={`${styles.common} ${styles.heading2}`} style={style}>
+          {children}
+        </h2>
       );
 
     case "h3":
       return (
-        <h3 className={`${styles.common} ${styles.heading3}`}>{children}</h3>
+        <h3 className={`${styles.common} ${styles.heading3}`} style={style}>
+          {children}
+        </h3>
       );
 
     case "h4":
       return (
-        <h4 className={`${styles.common} ${styles.heading4}`}>{children}</h4>
+        <h4 className={`${styles.common} ${styles.heading4}`} style={style}>
+          {children}
+        </h4>
       );
 
     case "h5":
       return (
-        <h5 className={`${styles.common} ${styles.heading5}`}>{children}</h5>
+        <h5 className={`${styles.common} ${styles.heading5}`} style={style}>
+          {children}
+        </h5>
       );
 
     case "body1":
-      return <p className={`${styles.common} ${styles.body1}`}>{children}</p>;
+      return (
+        <p className={`${styles.common} ${styles.body1}`} style={style}>
+          {children}
+        </p>
+      );
 
     case "body2":
-      return <p className={`${styles.common} ${styles.body2}`}>{children}</p>;
+      return (
+        <p className={`${styles.common} ${styles.body2}`} style={style}>
+          {children}
+        </p>
+      );
 
     default:
       return <p className={styles.common}>{children}</p>;
