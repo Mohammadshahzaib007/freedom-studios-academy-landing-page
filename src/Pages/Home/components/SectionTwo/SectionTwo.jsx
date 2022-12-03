90fc135e1e9a4e856cff8843973c4fc82d1a2dd0
Mohammad_shahzaib07 <shahzaib@quolam.com>
2022-12-04 00:00:02 +0530

    section two completed

--- src/Pages/Home/components/SectionTwo/SectionTwo.jsx
+++ src/Pages/Home/components/SectionTwo/SectionTwo.jsx
@@ -1,7 +1,37 @@
 import styles from "./sectionTwo.module.css";
+import img from "../../../../assets/2.png";
+import Text from "../../../../components/Text/Text";
+import CheckMarkIcon from "../../../../components/icons/CheckMarkIcon";
 
 function SectionTwo() {
-  return <section>SectionTwo</section>;
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <img src={img} alt="section img" />
+
+        <div className={styles.content}>
+          <Text type="h5" style={{ marginBottom: "5px" }}>
+            facilities
+          </Text>
+          <Text type="h2" style={{ marginBottom: "1.5625rem" }}>
+            world class trainning
+          </Text>
+
+          <div>
+            <CheckMarkIcon /> <Text type="h5">acces to Wifi</Text>
+          </div>
+
+          <div>
+            <CheckMarkIcon /> <Text type="h5">fully airconditioned</Text>
+          </div>
+
+          <div>
+            <CheckMarkIcon /> <Text type="h5">over 100 classes monthly</Text>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default SectionTwo;
