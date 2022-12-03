c5a187da01d405d14139728ed91989439deeaf2d
Mohammad_shahzaib07 <shahzaib@quolam.com>
2022-12-03 12:43:26 +0530

    added colors and basic btn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Button from "./components/Button/Button";
 import Text from "./components/Text/Text";
 
 function App() {
   return (
     <div className="App" style={{ background: "black", height: "100vh" }}>
       <Text type="h1">Shahzaib</Text>
+
+      <Button>Hello world</Button>
     </div>
   );
 }
